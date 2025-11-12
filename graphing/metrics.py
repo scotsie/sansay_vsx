a0dc5ccc389cbf56d603e77e745213d4a32d02cc
Scott C <31043147+scotsie@users.noreply.github.com>
2025-11-12 21:06:42 +0000

    Update graphing

--- graphing/metrics.py
+++ graphing/metrics.py
@@ -5,25 +5,55 @@
 
 # License: GNU General Public License v2
 
-from cmk.graphing.v1 import metrics, Title
+from cmk.graphing.v1.metrics import Color, DecimalNotation, Metric, StrictPrecision, Title, Unit, Product
+from cmk.graphing.v1.graphs import Graph
 
-metric_input_power = metrics.Metric(
-    name="input_power",
-    title=Title("Electrical input power"),
-    unit=metrics.Unit(metrics.DecimalNotation("Watt")),
-    color=metrics.Color.BROWN,
+unit_percent = Unit(
+    DecimalNotation("%"),
+    StrictPrecision(0)
 )
 
-metric_output_power = metrics.Metric(
-    name="output_power",
-    title=Title("Electrical output power"),
-    unit=metrics.Unit(metrics.DecimalNotation("Watt")),
-    color=metrics.Color.BLUE,
+
+metric_sansay_cpu_utilization = Metric(
+    name="cpu_utilization",
+    title=Title("CPU Utilization"),
+    unit=unit_percent,
+    color=Color.RED,
 )
 
-metric_input_voltage = metrics.Metric(
-    name="input_voltage",
-    title=Title("Electrical input voltage"),
-    unit=metrics.Unit(metrics.DecimalNotation("Volt")),
-    color=metrics.Color.GREEN,
+metric_sansay_session_utilization = Metric(
+    name="session_utilization",
+    title=Title("Session Utilization"),
+    unit=unit_percent,
+    color=Color.GREEN,
+)
+
+metric_sansay_session_utilization_drop = Metric(
+    name="session_utilization_drop",
+    title=Title("Session Utilization Drop"),
+    unit=unit_percent,
+    color=Color.YELLOW,
+)
+
+metric_sansay_session_utilization_trend = Product(
+    title=Title("Session Utilization Trend"),
+    unit=unit_percent,
+    color=Color.YELLOW,
+    factors=[
+        "metric_sansay_session_utilization",
+        "metric_sansay_session_utilization_drop"
+    ],
+)
+
+
+graph_sansay_vsx_system = Graph(
+    name="sansay_vsx_system",
+    title=Title("Sansay VSX System Metrics"),
+    simple_lines=[
+        "cpu_utilization",
+        "metric_sansay_session_utilization_trend"
+    ],
+    compound_lines=[
+        "session_utilization"
+    ],
 )
